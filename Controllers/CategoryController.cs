c284c6064dd7922a829b2adbbd944bc51ce8fdbb
willtruong236 <willtruong236@gmail.com>
2023-11-22 15:20:51 +1030

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using Books.Data;
+using Books.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext db;
+
+        public CategoryController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = db.Categories;
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                TempData["success"] = "Category created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Books.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
